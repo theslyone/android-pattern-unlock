8ddb6820a01e45086086a944e73b4dd537e4ee41
Sylvester <theslyguy@icloud.com>
2017-07-15 03:16:42 +0800

    deploy to heroku

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,5 +1,4 @@
 const logger = require('../lib/logger')
-
 logger.info('Starting server...')
 require('../../server/main').listen(process.env.PORT || 3000, () => {
   logger.success('Server is running at http://localhost:3000')
